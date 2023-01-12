eda9c5ae26611b7d5d61486cbad8aace16e79d20
LTH <johtta888@naver.com>
2023-01-12 22:47:30 +0900

    no

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import Router from "koa-router";
 import auth from "./auth";
-import me from "./me";
 import item from "./item";
+import me from "./me";
 
 const router = new Router();
 
 router.use("/auth", auth.routes());
-router.use("/me", me.routes());
 router.use("/item", item.routes());
+router.use("/me", me.routes());
 
 export default router;
